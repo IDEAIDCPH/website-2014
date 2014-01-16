db45b76f471fdba8278b3e33b6fb20c36ecac659
Emil Bay <hello@tixz.dk>
2014-01-16 12:19:31 +0100

    Added specific page to remove 'Apply Now' footer

---
+++ src/page-apply-now.php
@@ -0,0 +1,20 @@
+<?php
+
+get_header('default');
+
+/*
+Inherit "renderer.php"
+*/
+load_template( TEMPLATEPATH . '/renderer.php', false);
+
+/*
+Include call to action here
+*/
+?>
+
+		<aside class="well center">
+		</aside>
+	</main>
+	
+<?php get_footer('default'); ?>
+		
