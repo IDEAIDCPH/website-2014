f01523d77442485a9d5ae7305df0b8943e800e49
Emil Bay <hello@tixz.dk>
2014-01-24 00:22:56 +0100

    Removed cropping of six-cols images

--- src/functions.php
+++ src/functions.php
@@ -13,7 +13,7 @@ add_filter('show_admin_bar', '__return_false');
 
 add_image_size('left-right', 200, 155, true);
 add_image_size('three-cols', 370, 170, true);
-add_image_size('six-cols', 170, 100, true);
+add_image_size('six-cols', 170, 100, false);
 
 function get_default_template () {
 	return "template-part_description.php";
