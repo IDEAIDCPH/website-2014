0d8c0008aa1be1e531221e5b78b27f58d605fed8
Emil Bay <hello@tixz.dk>
2014-02-04 21:27:46 +0100

    Removed the title from six columns

--- src/template-part_six-collumns.php
+++ src/template-part_six-collumns.php
@@ -16,11 +16,10 @@ $args = array(
 <?php
 // Get pages with arguments and
 // loop through them
-foreach(get_pages($args) as $subpage) : 
+foreach(get_pages($args) as $subpage) :
 	$post = $subpage; ?>
 	<article class="one-of-six">
 		<?php the_post_thumbnail('six-cols'); ?>
-		<h1><?php echo $subpage->post_title; ?></h1>
 		<?php echo wpautop($subpage->post_content); ?>
 	</article>
 <?php endforeach; ?>
