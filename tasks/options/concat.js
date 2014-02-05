f8cb2caba370ab7ff3e10c54923404e6ee716947
Emil Bay <hello@tixz.dk>
2014-02-05 22:40:22 +0100

    Concat all js files

--- tasks/options/concat.js
+++ tasks/options/concat.js
@@ -6,7 +6,7 @@ module.exports = {
         src: [
             '<%= dirs.src.jsVendor + files.js %>',
             '<%= dirs.src.jsLibs + files.js %>',
-            '<%= dirs.src.js + "main.js" %>'
+            '<%= dirs.src.js + files.js %>'
         ],
         dest: '<%= dirs.tmp.js + "main.min.js" %>',
     }
